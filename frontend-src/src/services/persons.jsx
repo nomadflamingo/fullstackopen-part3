7d4f8a3a674cd434a4e94e1e853804b12667fc36
nomadflamingo <tetrenkuchioan@gmail.com>
2024-06-12 16:57:44 +0300

    (ex 3.21) fix api endpoint not set up correctly

--- frontend-src/src/services/persons.jsx
+++ frontend-src/src/services/persons.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = process.env.NODE_ENV === 'production'
+  ? '/api/persons'
+  : 'http://localhost:3001/api/persons'  // so that it works locally when only frontend is launched (wwhen using vite)
 
 const getAll = () => {
   const request = axios.get(baseUrl)
